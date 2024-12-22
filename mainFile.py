0255f6de4dcfbe115c5dda1d432463b7ad6129c4
Vanshaj Kaistha <kvanshaj@gmail.com>
2024-12-22 17:07:27 +0530

    Added function for operations

--- mainFile.py
+++ mainFile.py
@@ -8,5 +8,44 @@ def mainMenu():
     print("6. Increment")
     print("7. Decrement")
     print("0. Exit")
+#------------------------------------------------------------------------------------#
+
 while 1:
     mainMenu();
+
+#------------------------------------------------------------------------------------#
+def add():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 + num2
+    print("The sum of the two numbers is:", result)
+#------------------------------------------------------------------------------------#
+def sub():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 - num2
+    print("The sub of the two numbers is:", result)
+#------------------------------------------------------------------------------------#
+def multiply():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 * num2
+    print("The multiplication of the two numbers is:", result)
+#------------------------------------------------------------------------------------#
+def divide():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1/num2
+    print("The divided value of the two numbers is:", result)
+#------------------------------------------------------------------------------------#
+def mod():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1%num2
+    print("The mod of the two numbers is:", result)
+#------------------------------------------------------------------------------------#
+def increment():
+    num1 = float(input("Enter first number: "))
+    result = num1++
+    print("The incremented number is:", result)
+#------------------------------------------------------------------------------------#
