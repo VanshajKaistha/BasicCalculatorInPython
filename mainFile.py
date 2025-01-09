b218074ec96ce096b64a4cb08285abc7f9c6dbe1
Vanshaj Kaistha <kvanshaj@gmail.com>
2025-01-09 22:48:18 +0530

    mainFile.py

--- mainFile.py
+++ mainFile.py
@@ -1,3 +1,55 @@
+#------------------------------------------------------------------------------------#
+def add():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 + num2
+    print("The sum of the two numbers is:", result)
+    return result
+#------------------------------------------------------------------------------------#
+def sub():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 - num2
+    print("The sub of the two numbers is:", result)
+    return result
+#------------------------------------------------------------------------------------#
+def multiply():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1 * num2
+    print("The multiplication of the two numbers is:", result)
+    return result
+#------------------------------------------------------------------------------------#
+def divide():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1/num2
+    print("The divided value of the two numbers is:", result)
+    return result
+#------------------------------------------------------------------------------------#
+def mod():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    result = num1%num2
+    print("The mod of the two numbers is:", result)
+    return result
+#------------------------------------------------------------------------------------#
+def increment():
+    num1 = float(input("Enter first number: "))
+    result = num1+1
+    print("The incremented number is:", result)
+    return result
+#------------------------------------------------------------------------------------#
+def decrement():
+    num1 = float(input("Enter first number: "))
+    result = num1-1
+    print("The decremented number is:", result)
+    return result
+#------------------------------------------------------------------------------------#
+def exitFunc():#didnt used it yet could be used
+    print("Exiting Calculator")
+    return 
+#------------------------------------------------------------------------------------#
 def mainMenu():
     print("***Main Menu****")
     print("1. Addition")
@@ -8,44 +60,27 @@ def mainMenu():
     print("6. Increment")
     print("7. Decrement")
     print("0. Exit")
+    choice = int(input("Enter the operation you want to perform: "))
+
+    if choice == 1:
+        add()
+    elif choice == 2:
+         sub()
+    elif choice == 3:
+         multiply()
+    elif choice == 4:
+         divide()
+    elif choice == 5:
+         mod()
+    elif choice == 6:
+         increment()
+    elif choice == 7:
+         decrement()
+    elif choice == 0:
+         exit("""Exiting Calculator.
+Visit again :)""")
 #------------------------------------------------------------------------------------#
 
 while 1:
     mainMenu();
 
-#------------------------------------------------------------------------------------#
-def add():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    result = num1 + num2
-    print("The sum of the two numbers is:", result)
-#------------------------------------------------------------------------------------#
-def sub():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    result = num1 - num2
-    print("The sub of the two numbers is:", result)
-#------------------------------------------------------------------------------------#
-def multiply():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    result = num1 * num2
-    print("The multiplication of the two numbers is:", result)
-#------------------------------------------------------------------------------------#
-def divide():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    result = num1/num2
-    print("The divided value of the two numbers is:", result)
-#------------------------------------------------------------------------------------#
-def mod():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    result = num1%num2
-    print("The mod of the two numbers is:", result)
-#------------------------------------------------------------------------------------#
-def increment():
-    num1 = float(input("Enter first number: "))
-    result = num1++
-    print("The incremented number is:", result)
-#------------------------------------------------------------------------------------#
